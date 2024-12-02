991998d7acbe42b46b32856654ac3962eaefb21a
Arthur VIGOUROUX <vigouroux.art@gmail.com>
2024-12-02 09:45:33 +0100

    refacto is wall stuck
    refacto process of retrieving when character is stuck in a wall after teleportation

--- game/scripts/lilBoy.gd
+++ game/scripts/lilBoy.gd
@@ -3,23 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-
-const OUT_WALL_SPEED = 0.5
-
+const OUT_WALL_SPEED = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-@onready var _animated_sprite = $AnimSpriteLilBoy
-#var direction
 var portal:Portal
-
 var is_stuck_in_wall=false
 
 
-
-
+@onready var _animated_sprite = $AnimSpriteLilBoy
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -41,15 +33,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	#prevent inside wall teleportation
-	if is_on_wall() and not is_on_floor():
-		is_stuck_in_wall = true
-		position-=Vector2(0,OUT_WALL_SPEED)
-	# TODO: add damage and healing ?
-	else:
-		is_stuck_in_wall = false
+	_exit_wall_stuck(is_wall_stuck())
 	
-	print("stuck "+str(is_stuck_in_wall))
 	
 func _process(delta):
 	_animated_sprite.play("idle")
@@ -66,4 +51,25 @@ func _process(delta):
 			portal.queue_free()
 
 
+func is_wall_stuck():
+	#prevent inside wall teleportation
+	if is_on_wall() and not is_on_floor():
+		print("STUCK")
+		return true
+	else:
+		return false
 
+
+func _exit_wall_stuck(is_wall_stuck:bool):
+	if is_wall_stuck():
+		is_stuck_in_wall = true
+		$CollisionShape2D.disabled = true
+		position-=Vector2(0,OUT_WALL_SPEED)
+
+		# TODO: add damage and healing ?
+	else :
+		is_stuck_in_wall = false
+		$CollisionShape2D.disabled = false
+	
+	
+	
